72c985d082097d9bd6861d4a11b1e34113836de0
Mathias Vestergaard <2027834+mvestergaard@users.noreply.github.com>
2024-09-25 10:49:35 +0200

    Expand tests

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -5,22 +5,36 @@ namespace DataLoaderTest;
 public class UnitTest1
 {
     [Fact]
-    public async Task Test1()
+    public async Task ResultShouldBeEmptyWhenAllKeysAreMissing()
     {
-        var loader = new TestDataLoader(new AutoBatchScheduler(), new DataLoaderOptions());
+        var loader = new TestDataLoader(new Dictionary<int, string>(), new AutoBatchScheduler(), new DataLoaderOptions());
         var result = await loader.LoadRequiredAsync([1]);
         Assert.Empty(result);
     }
 
+    [Fact]
+    public async Task ResultShouldNotContainMissingItems()
+    {
+        var loader = new TestDataLoader(new Dictionary<int, string> { { 1, "foo" }, { 3, "bar" } },
+            new AutoBatchScheduler(), new DataLoaderOptions());
+        
+        var result = await loader.LoadRequiredAsync([1, 2, 3]);
+        Assert.NotNull(result[1]);
+        Assert.Equal(2, result.Count);
+    }
+
     private class TestDataLoader : BatchDataLoader<int, string>
     {
-        public TestDataLoader(IBatchScheduler batchScheduler, DataLoaderOptions options) : base(batchScheduler, options)
+        private readonly Dictionary<int, string> _dictionary;
+
+        public TestDataLoader(Dictionary<int, string> dictionary, IBatchScheduler batchScheduler, DataLoaderOptions options) : base(batchScheduler, options)
         {
+            _dictionary = dictionary;
         }
 
         protected override async Task<IReadOnlyDictionary<int, string>> LoadBatchAsync(IReadOnlyList<int> keys, CancellationToken cancellationToken)
         {
-            return await Task.FromResult(new Dictionary<int, string>().AsReadOnly());
+            return await Task.FromResult(_dictionary.AsReadOnly());
         }
     }
 }
